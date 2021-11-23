826bb01bf02c66d8a1e1dfb5d03b03fe381fae41
Ladun Omideyi <Ladun.Omideyi@softwire.com>
2021-11-23 14:24:05 +0000

    Add csproj file

---
+++ Chessington.GameEngine/Moves.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections.Generic;
+
+namespace Chessington.GameEngine
+{
+    public static class Moves
+    {
+        public static List<Square> GetLateralMoves(Square boardLocation, List<Square> legalMoves)
+        {
+            for (var i = 0; i < 8; i++)
+            {
+                legalMoves.Add(Square.At(boardLocation.Row, i));
+            }
+
+            for (var i = 0; i < 8; i++)
+            {
+                legalMoves.Add(Square.At(i, boardLocation.Col));
+            }
+            
+            //Get rid of our starting location.
+            legalMoves.RemoveAll(s => s == Square.At(boardLocation.Row, boardLocation.Col));
+            
+            return legalMoves;
+        }
+
+        public static List<Square> RemoveOutOfBoundsMoves(Square boardLocation, List<Square> legalMoves)
+        {
+            // add code to remove out of bounds moves
+            return legalMoves;
+        }
+    }
+}
