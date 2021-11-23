7c4befd7bc68eecf6df94c8cdb33b2090cb012bd
Ladun Omideyi <Ladun.Omideyi@softwire.com>
2021-11-23 13:08:39 +0000

    Pass test when pawns move two spaces

--- Chessington.GameEngine/Pieces/Pawn.cs
+++ Chessington.GameEngine/Pieces/Pawn.cs
@@ -1,4 +1,6 @@
-﻿using System.Collections.Generic;
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
 using System.Linq;
 
 namespace Chessington.GameEngine.Pieces
@@ -12,23 +14,30 @@ namespace Chessington.GameEngine.Pieces
         {
             var boardLocation = board.FindPiece(this);
             var legalMoves = new List<Square>();
-            GetLegalMoves(boardLocation, legalMoves);
-
+            legalMoves = GetLegalMoves(boardLocation, legalMoves);
             return legalMoves;
         }
 
-        private void GetLegalMoves(Square boardLocation, List<Square> legalMoves)
+        private List<Square> GetLegalMoves(Square boardLocation, List<Square> legalMoves)
         {
             switch (Player)
             {
-                case Player.White:     //moves down one square
+                case Player.Black when boardLocation.Row != 1:
+                    legalMoves.Add(new Square(boardLocation.Row + 1, boardLocation.Col));
+                    return legalMoves;
+                case Player.Black when boardLocation.Row == 1:
+                    legalMoves.Add(new Square(boardLocation.Row + 2, boardLocation.Col));
+                    legalMoves.Add(new Square(boardLocation.Row + 1, boardLocation.Col));
+                    return legalMoves;
+                case Player.White when boardLocation.Row != 7:
                     legalMoves.Add(new Square(boardLocation.Row - 1, boardLocation.Col));
-                    break;
-                case Player.Black:     //moves up one square
-                 legalMoves.Add(new Square(boardLocation.Row + 1, boardLocation.Col));
-                 break;
-        
+                    return legalMoves;
+                case Player.White when boardLocation.Row == 7:
+                    legalMoves.Add(new Square(boardLocation.Row - 2, boardLocation.Col));
+                    legalMoves.Add(new Square(boardLocation.Row - 1, boardLocation.Col));
+                    return legalMoves;
             }
+            return legalMoves;
         }
     }
 }
